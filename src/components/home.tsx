9015f4466dd5148fb4497420f543b1989cad425a
JorgoPapapostolu <jorgopapapostolu@gmail.com>
2023-03-20 23:35:59 +0100

    home, icon, title, routing

---
+++ src/components/home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="hero">
+      <div className="heading">
+        <div className="font-link">
+          <p className="drop-in">
+            Hello, I'm <span>Jorgos</span>.
+          </p>
+          <p className="drop-in-2">I'm a junior software developer.</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
